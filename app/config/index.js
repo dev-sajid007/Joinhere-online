8191fce39bb444f61829c64585e2e9ece5e7e1c8
Sajid Hasan <dev.sajid007@gmail.com>
2025-02-24 23:01:41 +0600

    changes config

--- app/config/index.js
+++ app/config/index.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://megaback-c4jx.vercel.app";
+export const API_URL = "https://mailhack.vercel.app";
 
-export const site = "joinhere-online.vercel.app/verify";
+export const site = "www.megapurrconal.site";
